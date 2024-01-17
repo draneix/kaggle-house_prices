f3785f4bfbec703ca9e9c1da9f402c65f34c6ec6
Aaron <aaronteoruikang@hotmail.com>
2024-01-17 21:50:31 +0800

    Added batch norm and more layers

--- mlp.py
+++ mlp.py
@@ -8,15 +8,26 @@ class MLP(nn.Module):
         super().__init__()
         # Input layer
         self.input1 = nn.Linear(input_size, 250)
+        # Batch norm
+        self.batch1 = nn.BatchNorm1d(250)
         # Dropout here
-        self.dropout1 = nn.Dropout(p=0.3)
+        self.dropout1 = nn.Dropout(p=0.1)
         # Hidden layer
         self.hidden1 = nn.Linear(250, 100)
+        self.batch2 = nn.BatchNorm1d(100)
+        self.hidden2 = nn.Linear(100, 50)
+        self.batch3 = nn.BatchNorm1d(50)
         # Output layer
-        self.output1 = nn.Linear(100, 1)
+        self.output1 = nn.Linear(50, 1)
 
     def forward(self, x):
-        x = F.tanh(self.input1(x))
+        x = self.input1(x)
+        x = self.batch1(x)
+        x = F.relu(x)
         x = self.dropout1(x)
-        x = F.relu(self.hidden1(x))
+        x = self.hidden1(x)
+        x = self.batch2(x)
+        x = F.relu(x)
+        x = self.hidden2(x)
+        x = self.batch3(x)
         return self.output1(x)
